334e68759e0427f1602f13a18b6c7dd64a0fda3e
reVENDRICKal <sa.mubark99@gmail.com>
2025-10-27 06:52:43 +0300

    lightEvent
    add LightEvent

--- Assets/SslmanScripts/EventsMNG/EventMNG.cs
+++ Assets/SslmanScripts/EventsMNG/EventMNG.cs
@@ -2,23 +2,44 @@ using UnityEngine;
 
 public class EventMNG : MonoBehaviour
 {
+
+    //sound event---------------------
     [SerializeField] OrderSys orderSys;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip SoundCallPlayer;
 
+
+
+
+    //Light event---------------------
+    [SerializeField] GameObject light;
+    [SerializeField] private AudioSource[] audios = new AudioSource[0];
+    [SerializeField] private AudioSource ForPower;
+    [SerializeField] private AudioClip PowerOff;
+    [SerializeField] private AudioClip PowerOn;
+
+
     //to be sure the event run one time 
     bool IsSoundEvent =false;
-
+    bool IsLightEvent = false;
 
     void Update()
     {
 
-        if (orderSys.OrderState == 5 && IsSoundEvent==false)
+        if (orderSys.OrderState == 5 && IsSoundEvent == false)
         {
             Invoke("SoundEvent", 2);
             IsSoundEvent = true;
         }
 
+        else if (orderSys.OrderState == 6 && IsLightEvent == false)
+        {
+            IsLightEvent = true;
+           LightEvent();
+
+
+
+        }
 
 
     }
@@ -27,9 +48,46 @@ public class EventMNG : MonoBehaviour
 
 
 
-     void SoundEvent()
+    void SoundEvent()
     {
         audioSource.PlayOneShot(SoundCallPlayer);
+
+    }
+    void LightEvent()
+    {
+        light.SetActive(false);
+        turnSoundOff();
+        ForPower.PlayOneShot(PowerOff);
+        Invoke("turnSoundOn" ,6);
+
+
+
+    }
+
+
+
+    void turnSoundOff()
+    {
+
+        foreach (var item in audios)
+        {
+            item.enabled = false;
+
+        }
+
+    }
+    
+     void turnSoundOn()
+    {
+        ForPower.PlayOneShot(PowerOn);
+
+        foreach (var item in audios)
+        {
+            item.enabled = true;
+
+        }
+
+        light.SetActive(true);
 
     }
 }
